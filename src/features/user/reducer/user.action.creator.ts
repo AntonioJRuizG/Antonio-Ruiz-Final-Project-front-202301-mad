5e3887a8327eee0e7ddd9c455fe046fdf41956b6
AntonioJesusRG <antoniogar129@gmail.com>
2023-03-14 19:40:48 +0100

    Add user reducer

---
+++ src/features/user/reducer/user.action.creator.ts
@@ -0,0 +1,13 @@
+import { createAction } from "@reduxjs/toolkit";
+import { UserStructure } from "../model/user.model";
+import { usersActions } from "./user.actions.types";
+
+export const loadCreator = createAction<UserStructure[]>(usersActions.load);
+
+export const addCreator = createAction<UserStructure>(usersActions.add);
+
+export const updateCreator = createAction<UserStructure>(usersActions.update);
+
+export const deleteCreator = createAction<UserStructure["id"]>(
+  usersActions.delete
+);
