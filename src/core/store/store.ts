161ea25cefca6736d372badab47666f9cf75064e
AntonioJesusRG <antoniogar129@gmail.com>
2023-03-14 20:02:49 +0100

    Add users reducer to store

--- src/core/store/store.ts
+++ src/core/store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { userReducer } from "../../features/user/reducer/user.reducer";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    users: userReducer,
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
