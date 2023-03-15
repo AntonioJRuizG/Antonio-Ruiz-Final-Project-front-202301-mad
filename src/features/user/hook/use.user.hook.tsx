b5234eb677e551ddcf69b9e0a18d9cd731f99784
AntonioJesusRG <antoniogar129@gmail.com>
2023-03-15 01:53:44 +0100

    Add user hook

---
+++ src/features/user/hook/use.user.hook.tsx
@@ -0,0 +1,35 @@
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../../core/store/store.js";
+import { ProtoUserStructure } from "../model/user.model.js";
+import { UserRepo } from "../services/repository/user.repo.js";
+import * as ac from "../reducer/user.action.creator";
+
+export function useUsers(repo: UserRepo) {
+  const users = useSelector((state: RootState) => state.users);
+
+  const dispatch = useDispatch<AppDispatch>();
+
+  const regUser = async (user: ProtoUserStructure) => {
+    try {
+      const newUser = await repo.registerUser(user);
+      dispatch(ac.addCreator(newUser));
+    } catch (error) {
+      console.log((error as Error).message);
+    }
+  };
+
+  const logUser = async (user: ProtoUserStructure) => {
+    try {
+      const newUser = await repo.loginUser(user);
+      dispatch(ac.addCreator(newUser));
+    } catch (error) {
+      console.log((error as Error).message);
+    }
+  };
+
+  return {
+    users,
+    regUser,
+    logUser,
+  };
+}
