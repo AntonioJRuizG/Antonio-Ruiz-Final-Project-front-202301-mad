eb03843c075e3c49a5fc30f624bf49d9d98de1f8
AntonioJesusRG <antoniogar129@gmail.com>
2023-03-16 15:21:37 +0100

    Add menu options type

--- src/core/component/app/app.tsx
+++ src/core/component/app/app.tsx
@@ -2,6 +2,16 @@ import { LoginForm } from "../../../features/user/components/login/login";
 import { RegisterForm } from "../../../features/user/components/register/register";
 import "./app.css";
 
+export type MenuOptions = {
+  label: string;
+  path: string;
+};
+
+export const menuOptions: MenuOptions[] = [
+  { label: "Register", path: "/" },
+  { label: "Login", path: "/about" },
+];
+
 export function App() {
   return (
     <>
