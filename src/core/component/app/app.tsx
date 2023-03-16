338a0d123f51ccef24fada3a8696d733e6a71453
AntonioJesusRG <antoniogar129@gmail.com>
2023-03-16 17:55:51 +0100

    Include navbar in app

--- src/core/component/app/app.tsx
+++ src/core/component/app/app.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRouter } from "../app.router/app.router";
+import { Navbar } from "../navbar/navbar";
 import "./app.css";
 
 export type MenuOptions = {
@@ -9,13 +10,14 @@ export type MenuOptions = {
 
 export const menuOptions: MenuOptions[] = [
   { label: "Register", path: "/" },
-  { label: "Login", path: "/about" },
+  { label: "Login", path: "/login" },
 ];
 
 export default function App() {
   return (
     <>
       <h1>Comunidad del bombardino</h1>
+      <Navbar menuOptions={menuOptions}></Navbar>
       <BrowserRouter>
         <AppRouter menuOptions={menuOptions}></AppRouter>
       </BrowserRouter>
