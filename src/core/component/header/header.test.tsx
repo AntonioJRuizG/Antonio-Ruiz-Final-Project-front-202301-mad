eb61c3f3f7ba16fb586fbd21fe39a0f784e59533
AntonioJesusRG <antoniogar129@gmail.com>
2023-03-16 18:26:55 +0100

    Test header component

---
+++ src/core/component/header/header.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+import { Header } from "./header";
+
+jest.mock("../navbar/navbar");
+describe("Given Header", () => {
+  describe("When it is render", () => {
+    test("Then it should be called", async () => {
+      render(
+        <Header>
+          <></>
+        </Header>
+      );
+      const element = screen.getByRole("heading");
+      expect(element).toBeInTheDocument();
+    });
+  });
+});
