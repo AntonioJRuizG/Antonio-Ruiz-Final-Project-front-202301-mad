e7a82bce6e401f2ead7f16aa1d107174cd92ac91
AntonioJesusRG <antoniogar129@gmail.com>
2023-03-17 18:04:24 +0100

    Add hook bombardino

---
+++ src/features/bombardino/hook/use.bombardino.hook.tsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../../store/store.js";
+import * as ac from "../reducer/bombardino.action.creator";
+import { BombardinoRepo } from "../services/repository/bombardino.repo.js";
+
+export function useBombardino(repo: BombardinoRepo) {
+  const bombardinos = useSelector((state: RootState) => state.bombardinos);
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    const loadBombardinos = async () => {
+      try {
+        const data = await repo.loadBombardinos();
+        dispatch(ac.loadCreator(data));
+      } catch (error) {
+        console.log((error as Error).message);
+      }
+    };
+    loadBombardinos();
+  }, [dispatch, repo]);
+
+  return { bombardinos };
+}
