5f50bcabd43d75a7cffad7d719c6e8a32129626b
AntonioJesusRG <antoniogar129@gmail.com>
2023-03-17 13:25:07 +0100

    Update store with bombardino reducer

--- src/core/store/store.ts
+++ src/core/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { bombardinoReducer } from "../../features/user/reducer/bombardino.reducer";
 import { userReducer } from "../../features/user/reducer/user.reducer";
 
 export const store = configureStore({
   reducer: {
     users: userReducer,
+    bombardinos: bombardinoReducer,
   },
 });
 export type AppDispatch = typeof store.dispatch;
