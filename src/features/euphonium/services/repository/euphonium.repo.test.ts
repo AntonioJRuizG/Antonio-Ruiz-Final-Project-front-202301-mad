e486b6de5d7a93aaf5193ebe38a2e4443f071ac8
AntonioJesusRG <antoniogar129@gmail.com>
2023-03-22 00:46:46 +0100

    Test create and update euphonium repo methods

--- src/features/euphonium/services/repository/euphonium.repo.test.ts
+++ src/features/euphonium/services/repository/euphonium.repo.test.ts
@@ -63,9 +63,53 @@ describe("Given EuphoniumRepo", () => {
       global.fetch = jest.fn().mockResolvedValue({
         ok: false,
         json: jest.fn().mockResolvedValue({}),
+        headers: {
+          "Content-type": "application/json",
+          Authorization: "Bearer testToken",
+        },
       });
       const result = euphoniumMockRepo.deleteEuphonium("1");
       await expect(result).rejects.toThrow();
+    });
+    test("Then data is added into local storage", () => {
+      localStorage.setItem("mockToken", "test-token");
+      expect(localStorage.getItem("mockToken")).toEqual("test-token");
+    });
+    test("Then token should be '' if there is no token in localStorage", () => {
+      const mockNullToken = null;
+      localStorage.setItem("mockToken", mockNullToken!);
+      expect(localStorage.getItem("mockToken")).toEqual("null");
+    });
+  });
+
+  describe("When createEuphonium method is called", () => {
+    test("Then it should fetch and create a new euphonium", async () => {
+      global.fetch = jest.fn().mockResolvedValue({
+        ok: true,
+        json: jest.fn().mockResolvedValue({
+          id: "1",
+          alias: "test",
+        }),
+      });
+      const result = await euphoniumMockRepo.createEuphonium({ alias: "test" });
+      expect(result).toEqual({ id: "1", alias: "test" });
+    });
+  });
+
+  describe("When updateEuphonium method is called", () => {
+    test("Then it should fetch and return de updated euphonium", async () => {
+      global.fetch = jest.fn().mockResolvedValue({
+        ok: true,
+        json: jest.fn().mockResolvedValue({
+          id: "1",
+          alias: "test-2",
+        }),
+      });
+      const result = await euphoniumMockRepo.updateEuphonium({
+        id: "1",
+        alias: "test-2",
+      });
+      expect(result).toEqual({ id: "1", alias: "test-2" });
     });
   });
 });
