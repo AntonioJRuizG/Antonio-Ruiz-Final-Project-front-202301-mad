459c46f5a53db81f111ca0677002661bd40fd2ca
AntonioJesusRG <antoniogar129@gmail.com>
2023-03-22 00:06:23 +0100

    Improve router test

--- src/router/app.router/app.router.test.tsx
+++ src/router/app.router/app.router.test.tsx
@@ -7,12 +7,14 @@ import { MenuOptions } from "../../app/app";
 jest.mock("../../common/components/gallery/gallery");
 jest.mock("../../features/user/components/login/login");
 jest.mock("../../features/user/components/register/register");
+jest.mock("../../pages/add/add.page");
 
-const pathsEntries = ["/", "/register", "/login"];
+const pathsEntries = ["/", "/add", "/register", "/login"];
 
 describe("Given AppRouter component", () => {
   const mockOptions: MenuOptions[] = [
     { label: "Gallery", path: "/" },
+    { label: "Add", path: "/add" },
     { label: "Register", path: "register" },
     { label: "Login", path: "/login" },
   ];
@@ -43,10 +45,23 @@ describe("Given AppRouter component", () => {
     });
   });
 
+  describe("When it is reder and the path is '/add'", () => {
+    test("Then the add form should be in the screen", async () => {
+      render(
+        <Router initialEntries={pathsEntries} initialIndex={2}>
+          <AppRouter menuOptions={mockOptions}></AppRouter>
+        </Router>
+      );
+
+      const element = await screen.findByRole("generic");
+      expect(element).toBeInTheDocument();
+    });
+  });
+
   describe("When it is render and the path is '/login'", () => {
     test("Then the login form should be in the screen", async () => {
       render(
-        <Router initialEntries={pathsEntries} initialIndex={2}>
+        <Router initialEntries={pathsEntries} initialIndex={3}>
           <AppRouter menuOptions={mockOptions}></AppRouter>
         </Router>
       );
