c16036f7f519343226f2637e0f463050450b29ba
AntonioJesusRG <antoniogar129@gmail.com>
2023-03-24 19:27:06 +0100

    Fix sonarCloud vulnerability

--- src/features/euphonium/services/firebase/firebase-config.ts
+++ src/features/euphonium/services/firebase/firebase-config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyD7RzCRPSkpbGOjyeb9SgGBp45_KUKLl0M",
-  authDomain: "final-project-euphoniums.firebaseapp.com",
-  projectId: "final-project-euphoniums",
-  storageBucket: "final-project-euphoniums.appspot.com",
-  messagingSenderId: "11895596067",
-  appId: "1:11895596067:web:d9cfe42584bd67fd6cbd96",
+  apiKey: process.env.DB_KEY,
+  authDomain: process.env.DB_DOMAIN,
+  projectId: process.env.DB_PI,
+  storageBucket: process.env.DB_SB,
+  messagingSenderId: process.env.DB_MSI,
+  appId: process.env.DB_APPID,
 };
