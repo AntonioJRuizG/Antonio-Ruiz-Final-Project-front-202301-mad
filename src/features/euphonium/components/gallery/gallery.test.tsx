cf273b681ea032b7ead03f1041f97a1c9ab09b57
AntonioJesusRG <antoniogar129@gmail.com>
2023-03-25 17:36:47 +0100

    Test gallery show more button

--- src/features/euphonium/components/gallery/gallery.test.tsx
+++ src/features/euphonium/components/gallery/gallery.test.tsx
@@ -19,18 +19,20 @@ describe("Given Gallery", () => {
   const mockEuphoniumRepo = {} as EuphoniumRepo;
 
   (useEuphonium as jest.Mock).mockReturnValue({
-    euphoniums: jest.fn(),
+    euphoniums: [],
     deleteEuphonium: jest.fn(),
+    loadEuphoniumsPaginated: jest.fn(),
   });
 
   (useUsers as jest.Mock).mockReturnValue({
-    users: jest.fn(),
+    users: [],
   });
 
   beforeEach(async () => {
     (useEuphonium as jest.Mock).mockReturnValue({
       euphoniums: [{ id: "1", creator: { id: "1" } }, { id: "2" }],
       deleteEuphonium: jest.fn(),
+      loadEuphoniumsPaginated: jest.fn(),
     });
 
     (useUsers as jest.Mock).mockReturnValue({
@@ -56,19 +58,30 @@ describe("Given Gallery", () => {
 
     describe("When click the first Link", () => {
       test("Then it should link to edit page", async () => {
-        const elements = screen.getAllByRole("button");
-        await userEvent.click(elements[0]);
-        expect(elements[0]).toBeInTheDocument();
+        const buttons = screen.getAllByRole("button");
+        await userEvent.click(buttons[0]);
+        expect(buttons[0]).toBeInTheDocument();
       });
     });
 
     describe("When click the second Link", () => {
       test("Then it should delete the item", async () => {
-        const elements = screen.getAllByRole("button");
-        await userEvent.click(elements[1]);
-        expect(elements[1]).toBeInTheDocument();
+        const buttons = screen.getAllByRole("button");
+        await userEvent.click(buttons[1]);
+        expect(buttons[1]).toBeInTheDocument();
         expect(
           useEuphonium(mockEuphoniumRepo).deleteEuphonium
+        ).toHaveBeenCalled();
+      });
+    });
+
+    describe("When click the third Link", () => {
+      test("Then it should calld the loadEuphoniumsPaginated", async () => {
+        const buttons = screen.getAllByRole("button");
+        await userEvent.click(buttons[2]);
+        expect(buttons[2]).toBeInTheDocument();
+        expect(
+          useEuphonium(mockEuphoniumRepo).loadEuphoniumsPaginated
         ).toHaveBeenCalled();
       });
     });
