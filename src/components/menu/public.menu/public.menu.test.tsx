af6a20b49cde4c13316bb95c8c60754e0b0bfe61
AntonioJesusRG <antoniogar129@gmail.com>
2023-03-27 23:58:16 +0200

    Test public menu

--- src/components/menu/public.menu/public.menu.test.tsx
+++ src/components/menu/public.menu/public.menu.test.tsx
@@ -1,24 +1,29 @@
-import { render } from "@testing-library/react";
-import { useUsers } from "../../../features/user/hook/use.user.hook";
-import { UserRepo } from "../../../features/user/services/repository/user.repo";
-import { Menu } from "../menu";
-
-jest.mock("../../../features/user/hook/use.user.hook");
-
-const repoUserMock = {} as UserRepo;
+/* eslint-disable testing-library/no-render-in-setup */
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { MenuOptions } from "../../../app/app";
+import { PublicMenu } from "./public.menu";
 
 describe("Given menu component", () => {
-  describe("When it renders there is a user logged", () => {
-    test("Then it should call logoutUser", () => {
-      (useUsers as jest.Mock).mockReturnValue({
-        user: {
-          user: { id: "1" },
-          token: "token-test",
-        },
-        logoutUser: jest.fn(),
-      });
-      render(<Menu></Menu>);
-      expect(useUsers(repoUserMock).logoutUser).toHaveBeenCalled();
+  describe("When it renders and no user is logged", () => {
+    const mockOptions: MenuOptions[] = [
+      {
+        id: "2",
+        label: "test",
+        path: "/test",
+      },
+    ];
+    beforeEach(async () => {
+      render(
+        <MemoryRouter>
+          <PublicMenu menuOptionsPublic={mockOptions}></PublicMenu>
+        </MemoryRouter>
+      );
+    });
+
+    test("Then it should render the PublicMenu", async () => {
+      const element = screen.getByText(mockOptions[0].label);
+      expect(element).toBeInTheDocument();
     });
   });
 });
