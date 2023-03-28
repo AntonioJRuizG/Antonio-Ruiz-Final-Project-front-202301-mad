34d836bcd5d2800b445d0daad76366ade7968a4f
AntonioJesusRG <antoniogar129@gmail.com>
2023-03-28 11:49:02 +0200

    Add current user component

---
+++ src/components/current.user/current.user.test.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable testing-library/no-render-in-setup */
+import { render, screen } from "@testing-library/react";
+import { useUsers } from "../../features/user/hook/use.user.hook";
+import { CurrentUserName } from "./current.user";
+
+jest.mock("../../features/user/hook/use.user.hook");
+
+describe("Given Header", () => {
+  describe("When it is render", () => {
+    beforeEach(async () => {
+      (useUsers as jest.Mock).mockReturnValue({
+        user: {
+          user: { id: "1" },
+        },
+      });
+      await render(<CurrentUserName></CurrentUserName>);
+    });
+
+    test("Then it should be called", async () => {
+      const element = screen.getAllByRole("generic");
+      expect(element[0]).toBeInTheDocument();
+    });
+  });
+});
