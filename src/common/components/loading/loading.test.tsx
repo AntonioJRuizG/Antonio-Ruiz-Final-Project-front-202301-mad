6e292ddd11bcd140a889ba36550a1f9e66820891
AntonioJesusRG <antoniogar129@gmail.com>
2023-04-29 15:49:28 +0200

    Fix code smell

--- src/common/components/loading/loading.test.tsx
+++ src/common/components/loading/loading.test.tsx
@@ -4,11 +4,11 @@ import { LoadingSpin } from "./loading";
 
 describe("Given the Loading component", () => {
   describe("When it is reder", () => {
-    beforeEach(async () => {
-      await render(<LoadingSpin></LoadingSpin>);
+    beforeEach(() => {
+      render(<LoadingSpin></LoadingSpin>);
     });
 
-    test("Then a generic div element should be in the document", async () => {
+    test("Then a generic div element should be in the document", () => {
       const element = screen.getAllByRole("generic");
       expect(element[0]).toBeInTheDocument();
     });
