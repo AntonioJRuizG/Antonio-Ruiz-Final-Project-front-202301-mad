2d0798b57340cd3fca36b629a7de52abb44c9a06
Antonio Ruiz <antonioruizgarcia90@gmail.com>
2023-09-06 05:07:20 +0200

    Change and test menu list

--- src/common/components/menu.list/menu.list.test.tsx
+++ src/common/components/menu.list/menu.list.test.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable testing-library/no-render-in-setup */
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
 import { MemoryRouter } from "react-router-dom";
 import { MenuOptions } from "../../../app/app";
@@ -9,6 +9,7 @@ describe("Given Menu component", () => {
   describe("When it renders", () => {
     const mockMenuOptions: MenuOptions[] = [
       { id: "1", label: "Galería", path: "/" },
+      { id: "2", label: "Other", path: "/other" },
     ];
 
     beforeEach(() => {
@@ -22,6 +23,14 @@ describe("Given Menu component", () => {
     test("Then it should be in the document", () => {
       const element = screen.getByText(mockMenuOptions[0].label);
       expect(element).toBeInTheDocument();
+    });
+
+    test("Then it should be possible to active hadle clic", () => {
+      const galleryLink = screen.getByText("Galería");
+      const otherLink = screen.getByText("Other");
+      fireEvent.click(galleryLink);
+      expect(galleryLink).toHaveClass("activeLink");
+      expect(otherLink).toHaveClass("mainMenuListLink");
     });
   });
 });
--- src/common/components/menu.list/menu.list.tsx
+++ src/common/components/menu.list/menu.list.tsx
@@ -2,17 +2,34 @@ import { Link } from "react-router-dom";
 import { MenuOptions } from "../../../app/app";
 
 import style from "./menu.list.style.module.scss";
+import { useState } from "react";
 
 export type NavProps = {
   menuOptions: MenuOptions[];
 };
 
 export function MenuList({ menuOptions }: NavProps) {
+  const [activeLink, setActiveLink] = useState("/");
+
+  const handleSetActiveLink = (path: string) => {
+    setActiveLink(path);
+    console.log(path);
+    console.log(activeLink);
+  };
+
   return (
     <>
       {menuOptions.map((item) => (
         <li key={item.id}>
-          <Link to={item.path} className={style.mainMenuListLink}>
+          <Link
+            to={item.path}
+            className={
+              style.mainMenuListLink +
+              " " +
+              (item.path === activeLink ? style["activeLink"] : "")
+            }
+            onClick={() => handleSetActiveLink(item.path)}
+          >
             {item.label}
           </Link>
         </li>
